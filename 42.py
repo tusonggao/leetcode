4bb863d56dd1614fd7fe553eab97295bdfe89199
涂松高 <tusonggao@163.com>
2018-10-18 23:44:48 +0800

    modified 42.py

--- 42.py
+++ 42.py
@@ -1,43 +1,49 @@
 class Solution:
-    def trap_range(self, height, left_index, right_index):
-        if left_index >= right_index:
+    def count_trap_volume(self, height, left_index, right_index):
+        if (left_index + 1) >= right_index:
             return 0
-        if left_index==(right_index-1):
-            return min(height[left_index], height[right_index])
+        return ((right_index-left_index-1)*min(height[left_index], height[right_index]) -
+                 sum(height[left_index+1:right_index]))
         
-        max_height = max(height[left_index:(right_index+1)])
-        for i in range(left_index, right_index+1):
-            if height[i]==max_height:
-                max_index_left = i
-                break
-        for i in range(right_index, left_index-1, -1):
-            if height[i]==max_height:
-                max_index_right = i
-                break
-        
-        max_height = -1
-        max_index_left_next = max_index_left
-        for i in range(max_index_left-1, left_index-1, -1):
-            if height[i] >= max_height:
-                max_index_left_next = i
-                max_height = height[i]
-        
-        max_height = -1
-        max_index_right_next = max_index_right
-        for i in range(max_index_right+1, right_index+1):
-            if height[i] >= max_height:
-                max_index_right_next = i
-                max_height = height[i]
-                
-        return ( (max_index_right - max_index_left)*max_height + 
-                 (max_index_left - max_index_left_next)*min(height[max_index_left], height[max_index_left_next]) +
-                 (max_index_right_next - max_index_right)*min(height[max_index_right_next], height[max_index_right]) +
-                 self.trap_range(height, left_index, max_index_left_next) + 
-                 self.trap_range(height, max_index_right_next, right_index) )
-    
     def trap(self, height):
         """
         :type height: List[int]
         :rtype: int
         """
-        return self.trap_range(height, 0, len(height)-1)
+        if len(height)<=1:
+            return 0
+        
+        trap_sum = 0        
+        max_height = max(height)
+        for i in range(0, len(height)):
+            if height[i]==max_height:
+                max_index_left = i
+                break
+        for i in range(len(height)-1, -1, -1):
+            if height[i]==max_height:
+                max_index_right = i
+                break
+        
+        trap_sum += self.count_trap_volume(height, max_index_left, max_index_right)
+        
+        current_max_height = height[0]
+        left_index = 0
+        cost_sum = height[0]
+        for i in range(1, max_index_left+1):
+            if current_max_height <= height[i]:
+                current_max_height = height[i]
+                right_index = i
+                trap_sum += self.count_trap_volume(height, left_index, right_index)
+                left_index = right_index
+        
+        current_max_height = height[-1]
+        right_index = len(height)-1
+        for i in range(len(height)-2, max_index_right-1, -1):
+            if current_max_height <= height[i]:
+                current_max_height = height[i]
+                left_index = i
+                trap_sum += self.count_trap_volume(height, left_index, right_index)
+                right_index = left_index
+                
+        return trap_sum
+    
