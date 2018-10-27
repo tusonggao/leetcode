1fd2ccac24a7610fd6fd5188ef6b504f8c99aa36
涂松高 <tusonggao@163.com>
2018-10-27 23:24:01 +0800

    add 137.py

---
+++ 137.py
@@ -0,0 +1,24 @@
+#使用O(n)空间的解法1
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        s = sum(set(nums)) * 3
+        ans = (s - sum(nums))//2
+        return ans
+		
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        from collections import Counter
+        counter = Counter(nums)
+        for val, num in counter.items():
+            if num != 3:
+                return val
