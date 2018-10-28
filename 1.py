9df76ce3c15058392e75c2ad30a659334bd6191a
涂松高 <tusonggao@163.com>
2018-10-28 20:10:15 +0800

    add 1.py

---
+++ 1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_idx = {}
+        for i in range(len(nums)):
+            nums_idx[nums[i]] = i
+        
+        for i in range(len(nums)):
+            val = target-nums[i]
+            if val in nums_idx and nums_idx[val]!=i:
+                return [i, nums_idx[val]]
+        
