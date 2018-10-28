84deb9dad50446eaa1d0d7438853ce3a4e2fe6ba
涂松高 <tusonggao@163.com>
2018-10-28 12:03:09 +0800

    add quick_sort.py

--- classic_algo/quick_sort.py
+++ classic_algo/quick_sort.py
@@ -1,23 +1,25 @@
 #快速排序的实现
 
 def quick_sort_r(nums, m, n):
-    if m > n:
+    if m >= n:
         return
 
-    val = nums[m]
-    low_idx = m
-    high_idx = n + 1
+    val = nums[m]  #pivot value
+    low_idx = m + 1
+    high_idx = n
 
     while low_idx < high_idx:
         if nums[high_idx] < val:
-            nums[high_idx], nums[low_idx] = nums[low_idx], nums[high_idx]
-            high_idx -= 1
+            nums[high_idx], val = val, nums[high_idx]
+        if nums[low_idx] > val:
+            nums[low_idx], val = val, nums[low_idx]
+        low_idx += 1
+        high_idx -= 1
 
-        if nums[high_idx] < val:
-            return
+    quick_sort_r(nums, m, low_idx-1)
+    quick_sort_r(nums, high_idx+1, n)
 
-    return nums
-
+    return
 
 def quick_sort(nums):
     quick_sort_r(nums, 0, len(nums)-1)
