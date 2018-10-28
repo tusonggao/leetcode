dd403ec8157c7b55042b2fd7d380a5bbf900dcf0
涂松高 <tusonggao@163.com>
2018-10-28 12:10:19 +0800

    add largest_all_1_submatrix.py

---
+++ hard_algo/largest_all_1_submatrix.py
@@ -0,0 +1,17 @@
+POJ3494——Largest Submatrix of All 1’s（栈的应用）
+
+Description
+
+Given a m-by-n (0,1)-matrix, of all its submatrices of all 1’s which is the largest? 
+By largest we mean that the submatrix has the most elements.
+
+Input
+
+The input contains multiple test cases. Each test case begins with m and n (1 ≤ m, n ≤ 2000) 
+on line. Then come the elements of a (0,1)-matrix in row-major order on m lines each with n 
+numbers. The input ends once EOF is met.
+
+Output
+
+For each test case, output one line containing the number of elements of the 
+largest submatrix of all 1’s. If the given matrix is of all 0’s, output 0.
