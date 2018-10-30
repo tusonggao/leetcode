12ceb3db9b0ebc42bbe3738511268e59cfa65611
涂松高 <tusonggao@163.com>
2018-10-30 13:37:43 +0800

    update 313.py

--- 313.py
+++ 313.py
@@ -21,5 +21,30 @@ class Solution:
             ugly_nums.append(num_min)
         return ugly_nums[-1]
 		
-#解法二：模型leetcode 264的解法，但是速度比较慢
+#解法二：使用堆队列，速度快
+
+class Solution:
+    def nthSuperUglyNumber(self, n, primes):
+        """
+        :type n: int
+        :type primes: List[int]
+        :rtype: int
+        """
+        import heapq
+        
+        heap, uglies, idx, ugly_by_last_prime = [], [0] * n, [0] * len(primes), [0] * n
+        uglies[0] = 1
+
+        for k, p in enumerate(primes):
+            heapq.heappush(heap, (p, k))
+
+        for i in range(1, n):
+            uglies[i], k = heapq.heappop(heap)
+            ugly_by_last_prime[i] = k
+            idx[k] += 1
+            while ugly_by_last_prime[idx[k]] > k:
+                idx[k] += 1
+            heapq.heappush(heap, (primes[k] * uglies[idx[k]], k))
+
+        return uglies[-1]
                     
