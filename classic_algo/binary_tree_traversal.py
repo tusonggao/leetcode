922f091762dc27d25b0014b94b3803840678692f
涂松高 <tusonggao@163.com>
2018-10-31 10:25:17 +0800

    update binary_tree_traversal.py

--- classic_algo/binary_tree_traversal.py
+++ classic_algo/binary_tree_traversal.py
@@ -52,3 +52,33 @@ class Solution:
 			return_list += self.postorderTraversal(root.right)
             return_list += [root.val]            
             return return_list
+			
+#层序遍历二叉树
+
+class Solution:
+    from collections import deque
+    def __init__(self):
+        self.nodeque = deque()
+        self.levelnodes = list()
+    
+    def levelGetNode(self, root, level_num):
+        if root is None:
+            return
+        else:
+            self.nodeque.append((root.val, level_num))
+            while self.nodeque:
+                self.nodeque.popleft(root)
+                self.levelnodes.append((root.val, level_num))
+                self.nodeque.append((root.val, level_num))
+                self.nodeque.append((root.val, level_num))
+        
+    def levelOrder(self, root):
+        
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        self.levelGetNodes(root, 0)
+            
+            
+        
